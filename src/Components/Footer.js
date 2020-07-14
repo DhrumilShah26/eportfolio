bfd29a957d27dca16c58e7b52bbefc83153d7f09
Dhrumil <sdhrumil2605@gmail.com>
2020-07-14 15:34:35 -0400

    footer done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
 
     if(this.props.data){
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank" rel="noopener noreferrer"><i className={network.className}></i></a></li>
       })
     }
 
@@ -19,8 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 <a title="Tim Baker" href="http://www.timbakerdev.com/">Tim Baker</a></li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2020 <a title="Dhrumil Shah" href="http://www.timbakerdev.com/">Dhrumil Shah</a></li>
            </ul>
 
         </div>
