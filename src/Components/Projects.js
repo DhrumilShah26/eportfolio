0ba883cf9c3dc10bd4a40c88a6fe391e43570954
Dhrumil <sdhrumil2605@gmail.com>
2020-08-11 19:15:36 -0400

    Projects updated

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,86 +2,128 @@ import React, { Component } from 'react';
 import TicTacToe from '../assets/projects/tic-tac-toe.png';
 import TwitterSA from '../assets/projects/twitter-sentiment-analysis.png';
 import WeatherFinder from '../assets/projects/weather-finder.png';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 class Projects extends Component {
   render() {
-
-    /*if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = projects.image;
-        return <div key={projects.title} className="columns projects-item">
-           <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="projects-item-meta">
-                    <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-                <div>{projects.title}</div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
-        </div>
-      })
-    }*/
 
     return (
       <section id="projects">
 
       <div className="row">
-
-         <div className="twelve columns collapsed">
-
-            <h1>Check Out Some of My Work.</h1>
-
-            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-
-              <div className="columns projects-item">
-                <a href='https://tictactoe-game-play.netlify.app/' title="Tic-Tac-Toe" target="_blank" rel="noopener noreferrer">
-                <img alt="Tic-Tac-Toe" src={TicTacToe} />
-                <div className="overlay">
-                  <div className="projects-item-meta">
-                    <h5>Tic-Tac-Toe</h5>
-                    <p>A react based Tic-Tac-Toe game</p>
-                  </div>
-                </div>
-                <div>Tic-Tac-Toe</div>
-                <div className="link-icon"><i className="fa fa-link"></i></div>
-                </a>
-              </div>
-
-              <div className="columns projects-item">
-                <a href='https://weather-finder-webapp.netlify.app/' title="Weather Finder" target="_blank" rel="noopener noreferrer">
-                <img alt="weather finder" src={WeatherFinder} />
-                <div className="overlay">
-                  <div className="projects-item-meta">
-                    <h5>Weather Finder</h5>
-                    <p>A React based weather finder app which fetch the data through API call</p>
-                  </div>
-                </div>
-                <div>Weather Finder</div>
-                <div className="link-icon"><i className="fa fa-link"></i></div>
-                </a>
-              </div>
-
-              <div className="columns projects-item">
-                <a href='https://github.com/DhrumilShah26/Twitter-sentiment-analysis' title="Twitter Sentiment Analysis" target="_blank" rel="noopener noreferrer">
-                <img alt="Twitter Sentiment Analysis" src={TwitterSA} />
-                <div className="overlay">
-                  <div className="projects-item-meta">
-                    <h5>Twitter Sentiment Analysis</h5>
-                    <p>Python based sentiment analysis using tweepy and textblob</p>
-                  </div>
-                </div>
-                <div>Twitter Sentiment Analysis</div>
-                <div className="link-icon"><i className="fa fa-link"></i></div>
-                </a>
-              </div>
-
-            </div>
-          </div>
+        <div className="twelve columns collapsed">
+          <h1>projects</h1>
+        <Accordion>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        <span className='heading'>Restaurant Management System </span>&nbsp; &nbsp;
+                        <a href="https://github.com/DhrumilShah26/Restaurant-Management-Front-End" target='_blank' rel="noopener noreferrer" title="front-end"><i className='fa fa-github link-icon'></i></a>
+                        &nbsp; &nbsp;
+                        <a href="https://github.com/DhrumilShah26/Restaurant-Management-Back-End" target='_blank' rel="noopener noreferrer" title="back-end"><i className='fa fa-github link-icon'></i></a>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>Technologies used: ReactJS, Redux, Bootstrap4, NodeJS, ExpressJS, MongoDB, Mongoose, Docker</p>
+                    <span>
+                      &bull; Developed a restaurant management system from which the user can login and can order various cuisines <br/>
+                      &bull; Implemented user authentication and verification using passport.js and JWT <br/>
+                      &bull; Database is stored in MongoDB collection using different database schemas <br/>
+                      &bull; Developed various RESTful API endpoints using ExpressJS router <br/>
+                    </span>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        <span className='heading'>Face-Mask-Detection </span>&nbsp; &nbsp;
+                        <a href="https://github.com/DhrumilShah26/Face-Mask-Detection" target='_blank' rel="noopener noreferrer" title="Source Code"><i className='fa fa-github link-icon'></i></a>
+                        </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>Technologies used: Python, Tensorflow, Keras, OpenCV, Pandas, Numpy, Scikit-Learn, Matplotlib</p>
+                    <span>
+                      &bull; Converted all the images into grayscale images using OpenCV and gave it to CNN as an input for training <br/>
+                      &bull; Implemented CNN model using Keras and Tensorflow libraries with 97% accuracy <br/>
+                      &bull; Live face detecting using OpenCV will give prediction whether a face is covered with a mask or not <br/>
+                    </span>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        <span className='heading'>Weather Finder </span>&nbsp; &nbsp;
+                        <a href="https://github.com/DhrumilShah26/Weather-API" target='_blank' rel="noopener noreferrer" title="Source Code"><i className='fa fa-github link-icon'></i></a>
+                        &nbsp;&nbsp;
+                        <a href="https://weather-finder-webapp.netlify.app/" target='_blank' rel="noopener noreferrer" title="Demo"><i className='fa fa-link link-icon'></i></a>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>Technologies used: ReactJS, Bootstrap4</p>
+                    <span>
+                      &bull; Fetches the weather information through an API call of openweathermap.org <br/>
+                      &bull; Successfully render the information into React component <br/>
+                    </span>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        <span className='heading'>Tic-Tac-Toe Game </span>&nbsp; &nbsp;
+                        <a href="https://github.com/DhrumilShah26/Tic-Tac-Toe" target='_blank' rel="noopener noreferrer" title="Source Code"><i className='fa fa-github link-icon'></i></a>
+                        &nbsp; &nbsp;
+                        <a href="https://tictactoe-game-play.netlify.app/" target='_blank' rel="noopener noreferrer" title="Demo"><i className='fa fa-link link-icon'></i></a>
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>Technologies used: ReactJS, Bootstrap4</p>
+                    <span>
+                      &bull; Developed two player tic-tac-toe game using React for learning purpose <br/>
+                      &bull; Learned basic concepts of ReactJS such as Components and Props, State and LifeCycle, Handling Events, Rendering Elements <br/>
+                    </span>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        <span className='heading'>Web Search Engine </span>&nbsp; &nbsp;
+                        <a href="https://github.com/DhrumilShah26/Web-Search-Engine" target='_blank' rel="noopener noreferrer" title="Source Code"><i className='fa fa-github link-icon'></i></a>
+                        </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>Technologies used: Java, Python</p>
+                    <span>
+                      &bull; Developed a Web Search Engine in Java <br/>
+                      &bull; Implemented features such as Web Crawler, Pattern Matching, Html To Text, Inverted Index and Page Ranking <br/>
+                      &bull; Concepts used - Depth First Search (DFS), Regular Expression, Jsoup Library, HashMap, Sorting <br/>
+                    </span>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        <span className='heading'>Twitter Sentiment Analysis </span>&nbsp; &nbsp;
+                        <a href="https://github.com/DhrumilShah26/Twitter-sentiment-analysis" target='_blank' rel="noopener noreferrer" title="Source Code"><i className='fa fa-github link-icon'></i></a>
+                        </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                  <p>Technologies used: Python, Matplotlib</p>
+                    <span>
+                      &bull; Extracted live Twitter textual data to understand the real sentiment of those tweets using tweepy library <br/>
+                      &bull; Classified tweets into three categories Positive, Negative and Neutral using textblob library <br/>
+                      &bull; Visualized all the results combined in different charts using matplotlib library <br/>
+                    </span>
+                </AccordionItemPanel>
+            </AccordionItem>
+        </Accordion>
+        </div>       
       </div>
     </section>
   );
